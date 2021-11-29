79a2240d3d5de30435332ce1c3d2b85e6bf215b7
tobikars <tobikars@gmail.com>
2021-11-29 17:48:39 +0800

    logs with winston

--- app.js
+++ app.js
@@ -1,41 +1,58 @@
 const express = require('express')
 const path = require('path')
-const morgan = require('morgan')
+const fs = require('fs')
 
+const morgan = require('morgan')
 const winston = require('winston')
-require('winston-syslog').Syslog
+
+const { createLogger, format } = winston
 
 const LOG_PATH = 'logs/app_log'
 
-const logger = winston.createLogger({
-  levels: winston.config.syslog.levels,
-  transports: [
-    new winston.transports.Syslog(),
-    new winston.transports.File({
-      filename: LOG_PATH,
-    }),
-  ],
-})
-
 const app = express()
 
-app.use(morgan('dev'))
+// setup the logger
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, LOG_PATH), { flags: 'a' })
+app.use(morgan('combined', { stream: accessLogStream }))
 
+function createAppLogger() {
+  const { combine, timestamp, printf, colorize } = format
+
+  return createLogger({
+    level: 'info',
+    format: combine(
+      colorize(),
+      timestamp(),
+      printf((info) => {
+        return `[${info.timestamp}] [${info.level}] : ${JSON.stringify(info.message)}`
+      })
+    ),
+    transports: [
+      new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'logs/combined.log' }),
+    ],
+  })
+}
+
+const console = createAppLogger()
+
+// serve static files from the public folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' })
-  logger.info('just a request to root.')
+  console.info('just a request to root.')
 })
 
 app.get('/error_500', (req, res) => {
   res.status(500).send({ message: 'Server error' })
-  logger.error('internal server error')
+  console.error('internal server error')
 })
 
 app.get('/error_400', (req, res) => {
   res.status(400).send({ message: 'Bad request' })
-  logger.warning('bad request')
+  console.warning('bad request')
 })
 
 module.exports = app
