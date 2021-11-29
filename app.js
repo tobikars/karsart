39cf5abdd965acce290e003777ba55c9ca0bdac2
tobikars <tobikars@gmail.com>
2021-11-29 14:43:05 +0800

    added logs

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
+
+const winston = require('winston')
+require('winston-syslog').Syslog
+
+const LOG_PATH = 'logs/app.log'
+
+const logger = winston.createLogger({
+  levels: winston.config.syslog.levels,
+  transports: [
+    new winston.transports.Syslog(),
+    new winston.transports.File({
+      filename: LOG_PATH,
+    }),
+  ],
+})
 
 const app = express()
+
+app.use(morgan('dev'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({ message: 'Hello World!' })
+  logger.info('just a request to root.')
+})
+
+app.get('/error_500', (req, res) => {
+  res.status(500).send({ message: 'Server error' })
+  logger.error('internal server error')
+})
+
+app.get('/error_400', (req, res) => {
+  res.status(400).send({ message: 'Bad request' })
+  logger.warning('bad request')
 })
 
 module.exports = app
